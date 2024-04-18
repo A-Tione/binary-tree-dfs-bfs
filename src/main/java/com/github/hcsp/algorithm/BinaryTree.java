ac561dfba8b08ee826905c67927e4639725326ce
Yang.he <yang.he@homepartners.tech>
2024-04-18 17:30:07 +0800

    - [x] completed "Breadth-First Search" and "Deep-First Search"

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,7 +1,6 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTree {
     public static void main(String[] args) {
@@ -26,12 +25,36 @@ public class BinaryTree {
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> list = new LinkedList<>();
+
+        list.offer(root);
+        while (!list.isEmpty()) {
+            TreeNode node = list.poll();
+            result.add(node.value);
+            if (node.left != null) {
+                list.offer(node.left);
+            }
+            if (node.right != null) {
+                list.offer(node.right);
+            }
+        }
+
+        return result;
     }
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        dfsHelper(root, result);
+        return result;
+    }
+
+    private static void dfsHelper(TreeNode node, List<Integer> result) {
+        if (node == null) {return;}
+        result.add(node.value);
+        dfsHelper(node.left, result);
+        dfsHelper(node.right, result);
     }
 
     public static class TreeNode {
